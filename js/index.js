98412f5edae6690630dfc715be3cf02c7dfe6973
Rafal Leszczynski <info@grafikart.pl>
2018-09-17 19:55:57 +0200

    Modal version

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
-var playerScore = 0;
-var compScore = 0;
-var draw = 0;
-var rounds = 0;
-var countRounds = 0;
-var endMatch = 0;
+var compChoice;
+var userChoice; 
 var gameover = 'Game over, please press the new game button!';
 
-//czekaj na zdarzenie związane z kliknęciem boxa 
-document.getElementById('box_rock').onclick=function playRock() {playMove('rock')};
-document.getElementById('box_paper').onclick=function playPaper() {playMove('paper')}
-document.getElementById('box_scissor').onclick=function playScissors() {playMove('scissor')}
-document.getElementById('NewGame').onclick=startNewGame;
-  
+var params = {
+  playerScore: 0,
+  compScore: 0, 
+  draw: 0, 
+  rounds: 0, 
+  countRounds: 0, 
+  endMatch: 0,
+  progress: []};
+  console.log(params.compScore);
+
+var buttonChoice = document.querySelectorAll('.player-move');
+	  for(var i = 0; i < buttonChoice.length; i++){
+    buttonChoice[i].addEventListener('click', function(event) { 
+        var gameButtonAtribute = event.target.getAttribute("data-move");
+        playMove(gameButtonAtribute);
+        });
+      }
+
+//czekaj na wciśnięcie przyciusku start i uruchom grę
+    var modalLinks = document.getElementById('NewGame');
+    for(var i = 0; i < modalLinks.length; i++){
+        modalLinks[i].addEventListener('click', startNewGame);
+    }
+
+// function getDateFromModal () {
+//     document.querySelector('#modal-overlay').classList.add('show');
+//     document.querySelector("#modal-start").classList.add('show');
+//     params.countRounds = document.getElementById("countRounds");
+//     console.log(params.countRounds);
+// }
+
+
 //funkcja losowania wyboru komputera
 function getCompChoice() {
   var compChooses = Math.floor(Math.random() * 3 + 1);
@@ -20,40 +42,80 @@ function getCompChoice() {
 
 //funkcja start nowej gry i resetu
 function startNewGame () {
-  countRounds = window.prompt('How many rounds do you wanna play?')
-  playerScore = 0;
-  compScore = 0;
-  rounds = 0;
-  draw = 0;
-  document.getElementById('playerScore').innerHTML = playerScore;
-  document.getElementById('compScore').innerHTML = compScore;
-  document.getElementById('rounds').innerHTML = rounds;
+  newTable()
+  params.countRounds = window.prompt('How many rounds do you wanna play?')
+
+  // document.querySelector('#modal-overlay').classList.add('show');
+  // document.querySelector("#modal-start").classList.add('show');
+  // params.countRounds = document.getElementById("countRounds");
+  // console.log(params.countRounds);
+
+
+  params.playerScore = 0;
+  params.compScore = 0;
+  params.rounds = 0;
+  params.draw = 0;
+  // document.getElementById('playerScore').innerHTML = params.playerScore;
+  // document.getElementById('compScore').innerHTML = params.compScore;
+  // document.getElementById('rounds').innerHTML =  params.rounds;
   document.getElementById('playerResult').innerHTML = '';
   document.getElementById('compResult').innerHTML = '';
   document.getElementById('winner').innerHTML = '';
-  document.getElementById('draw').innerHTML = '0';
+  // document.getElementById('draw').innerHTML = '0';
   document.getElementById('winnerGame').innerHTML = '';
   document.getElementById('info_play').innerHTML = '';
-  if (countRounds == 0 || countRounds == null || isNaN(countRounds)) {
+  console.log(params.countRounds);
+  if (params.countRounds == 0 || params.countRounds == null || isNaN(params.countRounds)) {
   document.getElementById('winnerGame').innerHTML = 'Enter correct data';
   }
   else {
     document.getElementById('info_play').innerHTML = 'You can play - make the first move';
-    document.getElementById('countRounds').innerHTML = countRounds;
+    // document.getElementById('countRounds').innerHTML = params.countRounds;
   }
 
 };
 
-//funkcja sprawdzająca czy jest koniec gry
+//funkcja czyszczenia tabeli
+function newTable() {
+	var table = document.getElementById("tablescore");
+	var rowNumbers = document.getElementById("tablescore").rows.length;
+	for (var i = 1; i < rowNumbers; i++) {
+		table.deleteRow(1);
+	}
+}
+
+
+//funkcja sprawdzająca czy jest koniec gry i uruchamiająca modal
 function endofmatch () {
-      if (rounds == countRounds && playerScore > compScore){
-      document.getElementById('winnerGame').innerHTML = 'You won the entire game!<br><br>' + gameover;
+    document.querySelector('#modal-overlay').classList.add('show');
+    document.querySelector("#modal-one").classList.add('show');  
+    
+    if (params.rounds == params.countRounds && params.playerScore > params.compScore){
+      // document.getElementById('winnerGame').innerHTML = 'You won the entire game!<br><br>' + 'gameover';
+
+      var el = document.createElement("div");
+      el.id = "myDiv";
+      el.innerText = 'You won the entire game!';
+      el.style.backgroundColor = "FF6633";    
+      var div = document.querySelector(".txt_modal1"); //pobieramy miejsce docelowe
+      div.appendChild(el); //wstawiamy element do drzewa dokumentu
       }   
-      else if (rounds == countRounds && playerScore < compScore){
-     document.getElementById('winnerGame').innerHTML = 'Computer won!<br><br>' + gameover;
-             }
-      else if (rounds == countRounds && playerScore == compScore) {
-     document.getElementById('winnerGame').innerHTML = 'Match for a draw!<br><br>' + gameover;
+      
+      else if (params.rounds == params.countRounds && params.playerScore < params.compScore){
+      var el = document.createElement("div");
+      el.id = "myDiv";
+      el.innerText = "Computer won!";
+      el.style.backgroundColor = "FF6633";    
+      var div = document.querySelector(".txt_modal1");
+      div.appendChild(el);
+    }
+      else if (params.rounds == params.countRounds && params.playerScore == params.compScore) {
+      var el = document.createElement("div");
+      el.id = "myDiv";
+      el.innerText = "Match for a draw!";
+      el.style.backgroundColor = "FF6633";    
+      var div = document.querySelector(".txt_modal1");
+      div.appendChild(el);
      }
   };
 
@@ -61,12 +123,12 @@ function endofmatch () {
 // funkcja wywołująca grę
 function playMove(userChoice) {
   //sprawdź czy podano liczbę gier
-  if (countRounds == 0) {
+  if (params.countRounds == 0 || params.countRounds == null || isNaN(params.countRounds)) {
   document.getElementById('winnerGame').innerHTML = 'Press the button: New game';
 } 
 
   //sprawdz czy nie koniec gry - jeśli tak wywołaj funkcję
-else if (countRounds == rounds) {
+else if (params.countRounds == params.rounds) {
   endofmatch ();
 } 
   
@@ -87,31 +149,148 @@ else {
       
   //spradź kto wygrał
   if(userChoice == compChoice){
-  winner = null;
+  // winner = null;
+  var roundwinner = "draw";
+
   document.getElementById('winner').innerHTML = "It is a draw!";
-  draw++;
-  rounds++;
+  params.draw++;
+  params.rounds++;
+  
+  var currentRound = {
+    playerScore: params.playerScore,
+    compScore: params.compScore,
+    rounds: params.rounds,
+    draw: params.draw,
+    userC: userChoice,
+    compC: compChoice,
+    roundW: roundwinner
   }
+  // console.log(userChoice);
+  // console.log(compChoice);
+  // console.log(roundwinner);
+  params.progress.push(currentRound);
+  showResult()
+}
+
   else if((userChoice == 'rock' && compChoice == 'paper') || (userChoice == 'paper' && compChoice == 'scissor') || (userChoice == 'scissor' && compChoice == 'rock')){
   document.getElementById('winner').innerHTML = "Computer win!";
-  compScore++;
-  rounds++;
+  var roundwinner = "Computer";
+  params.compScore++;
+  params.rounds++;
+  var currentRound = {
+    playerScore: params.playerScore,
+    compScore: params.compScore,
+    rounds: params.rounds,
+    draw: params.draw,
+    userC: userChoice,
+    compC: compChoice,
+    roundW: roundwinner
+  }
+  params.progress.push(currentRound);
+  showResult()
   }
   else {
-  document.getElementById('winner').innerHTML = "You win!"; 
-  playerScore++;
-  rounds++;
+  document.getElementById('winner').innerHTML = "You win!";
+  var roundwinner = "Player"; 
+  params.playerScore++;
+  params.rounds++;
+  var currentRound = {
+    playerScore: params.playerScore,
+    compScore: params.compScore,
+    rounds: params.rounds,
+    draw: params.draw,
+    userC: userChoice,
+    compC: compChoice,
+    roundW: roundwinner
   }
- 
-  // podaj wyniki gry i liczbę rund
-  document.getElementById('playerScore').innerHTML = playerScore;
-  document.getElementById('compScore').innerHTML = compScore;
-  document.getElementById('draw').innerHTML = draw;
+  params.progress.push(currentRound);
+  showResult()
+  }
+  
   //jeśli ostatnia runda wywołaj funkcję
-  if (countRounds == rounds){
-      document.getElementById('rounds').innerHTML = rounds;
+  if (params.countRounds == params.rounds){
       endofmatch ();
   }
-  document.getElementById('rounds').innerHTML = rounds;
+  // document.getElementById('rounds').innerHTML = params.rounds;
  }
 };
+
+
+//funkcja wstawiająca wyniki gry do tablicy
+function showResult() {
+  var x = params.rounds-1;
+  console.log(x);
+  console.log(params.progress[x].compC);
+  console.log(params.progress[x].userC);
+
+	var table = document.getElementById("tablescore");
+  var row = table.insertRow(params.rounds);
+	var RoundTab = row.insertCell(0);
+	var playerMoveTab = row.insertCell(1);
+  var computerMoveTab = row.insertCell(2);
+  var roundWin = row.insertCell(3);
+  var roundresultTab = row.insertCell(4);
+  // console.log(params.progress.compChoice);
+	RoundTab.insertAdjacentHTML('beforeend', params.rounds);
+	playerMoveTab.insertAdjacentHTML('beforeend', params.progress[x].userC);
+  computerMoveTab.insertAdjacentHTML('beforeend', params.progress[x].compC);
+  roundWin.insertAdjacentHTML('beforeend', params.progress[x].roundW);
+  roundresultTab.insertAdjacentHTML('beforeend', params.playerScore + " - " + params.compScore);
+}
+
+//modale 
+
+var showModal = function(event){
+  event.preventDefault();		
+  
+  //znajdz aktywny modal  
+  var activeModalLink = event.target.getAttribute('href');
+    
+ //dodanie klasy "show" do aktywnego modala
+  document.querySelector('#modal-overlay').classList.add('show');
+  document.querySelector(activeModalLink).classList.add('show');     
+  };
+
+  var allModalLinks = document.querySelectorAll('.show-modal');
+  for(var i = 0; i < allModalLinks.length; i++){
+  allModalLinks[i].addEventListener('click', showModal);
+  }
+
+// Dodajemy też funkcję zamykającą modal, oraz przywiązujemy ją do kliknięć na elemencie z klasą "close". 
+
+  var hideModal = function(event){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');  	
+  
+
+  var element = document.getElementById('txt_modal1');
+  console.log(element);
+  while (element.firstChild) {
+  element.removeChild(element.firstChild);
+  }
+
+  //usunięcie klasy "show" z modali
+  var allModal = document.querySelectorAll('.modal');
+  var n;
+    for (n = 0; n < allModal.length; n++) {
+        allModal[n].classList.remove('show');
+    }
+  };	
+
+  var closeButtons = document.querySelectorAll('.modal .close');	
+  for(var i = 0; i < closeButtons.length; i++){
+    closeButtons[i].addEventListener('click', hideModal);
+  }
+
+// zamnknięcie - kliknięcie w overlay 
+
+  document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
+// propagację kliknięć z modala 
+
+  var modals = document.querySelectorAll('.modal');	
+  for(var i = 0; i < modals.length; i++){
+  modals[i].addEventListener('click', function(event){
+  event.stopPropagation();
+  });
+}
